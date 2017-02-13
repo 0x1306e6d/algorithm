26ef89ac07d8c9ae1570723ac8d10aaddc96ff85
Gihwan Kim <ghkim3221@gmail.com>
2017-02-13 22:44:45 +0900

    5724 : 파인만  풀이

---
+++ baekjoon/5724.cpp
@@ -0,0 +1,39 @@
+/*
+    5724 : 파인만
+    URL : https://www.acmicpc.net/problem/5724
+    Input :
+        2
+        1
+        8
+        0
+    Output :
+        5
+        1
+        204
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        int N;
+        cin >> N;
+        if (N == 0)
+        {
+            break;
+        }
+        else
+        {
+            int sum = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                sum += (i * i);
+            }
+            cout << sum << endl;
+        }
+    }
+    return 0;
+}
