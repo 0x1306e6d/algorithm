d18b1c5cbc2eaa13a07cde24f6da5cef410ac246
Gihwan Kim <ghkim3221@gmail.com>
2017-03-25 16:48:01 +0900

    11782 : 그대로 출력하기 풀이

---
+++ baekjoon/11718.py
@@ -0,0 +1,17 @@
+"""
+    11718 : 그대로 출력하기
+    URL : https://www.acmicpc.net/problem/11718
+    Input :
+        Hello
+        Baekjoon
+        Online Judge
+    Output :
+        Hello
+        Baekjoon
+        Online Judge
+"""
+while True:
+    s = input()
+    if (len(s) == 0):
+        break
+    print(s)
