3ba4b036c52e414dbd005d7e6185cb4beca95634
Gihwan Kim <ghkim3221@gmail.com>
2017-03-25 14:09:11 +0900

    1000 : A + B 풀이

---
+++ baekjoon/1000.py
@@ -0,0 +1,10 @@
+"""
+    1000 : A + B
+    URL : https://www.acmicpc.net/problem/1000
+    Input : 1 2
+    Output : 3
+"""
+input = input().split()
+a = int(input[0])
+b = int(input[1])
+print(a + b)
