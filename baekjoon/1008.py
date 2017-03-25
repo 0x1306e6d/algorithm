a0d4b2aac87b94ad75cf86955e8f792d298bbf09
Gihwan Kim <ghkim3221@gmail.com>
2017-03-25 14:18:56 +0900

    1008 : A / B 풀이

---
+++ baekjoon/1008.py
@@ -0,0 +1,16 @@
+"""
+    1008 : A / B
+    URL : https://www.acmicpc.net/problem/1008
+    Input #1 :
+        1 3
+    Output #1 :
+        0.33333333333333333333333333333333
+    Input #1 :
+        4 5
+    Output #1 :
+        0.8
+"""
+input = input().split()
+a = int(input[0])
+b = int(input[1])
+print(a / b)
