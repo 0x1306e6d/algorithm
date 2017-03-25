afdeba2d5f3558c0bdd2beec8227c6716daa981c
Gihwan Kim <ghkim3221@gmail.com>
2017-03-25 14:19:05 +0900

    1001 : A - B 풀이

---
+++ baekjoon/1001.py
@@ -0,0 +1,12 @@
+"""
+    1001 : A - B
+    URL : https://www.acmicpc.net/problem/1001
+    Input :
+        3 2
+    Output :
+        1
+"""
+input = input().split()
+a = int(input[0])
+b = int(input[1])
+print(a - b)
