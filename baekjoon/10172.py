cee838070f397e4eef35f6442cf4af0ef536ed6b
Gihwan Kim <ghkim3221@gmail.com>
2019-01-02 22:48:18 +0900

    10172 : 개 풀이

---
+++ baekjoon/10172.py
@@ -0,0 +1,11 @@
+"""
+    10172 : 개
+    URL : https://www.acmicpc.net/problem/10172
+    Input :
+    Output :
+"""
+print('|\\_/|')
+print('|q p|   /}')
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
