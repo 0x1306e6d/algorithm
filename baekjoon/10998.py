f499a1e64ed7cc75eccfb929ebdfc4a6aa49eb63
Gihwan Kim <ghkim3221@gmail.com>
2019-01-02 22:58:32 +0900

    10998 : A×B 풀이

---
+++ baekjoon/10998.py
@@ -0,0 +1,16 @@
+"""
+    10998 : A×B
+    URL : https://www.acmicpc.net/problem/10998
+    Input #1 :
+        1 2
+    Output #1 :
+        2
+    Input #2 :
+        3 4
+    Output #3 :
+        12
+"""
+AB = input().split()
+A = int(AB[0])
+B = int(AB[1])
+print((A * B))
