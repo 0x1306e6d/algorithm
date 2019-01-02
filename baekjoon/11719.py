044489a7e229b64dc06192f42f98f5033975b162
Gihwan Kim <ghkim3221@gmail.com>
2019-01-02 21:17:12 +0900

    11719 : 그대로 출력하기 2 풀이

---
+++ baekjoon/11719.py
@@ -0,0 +1,19 @@
+"""
+    11719 : 그대로 출력하기 2
+    URL : https://www.acmicpc.net/problem/11719
+    Input :
+            Hello
+
+        Baekjoon     
+            Online Judge  
+    Output :
+            Hello
+
+        Baekjoon     
+            Online Judge  
+"""
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break
