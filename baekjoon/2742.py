8cb1b4692eb689611d9c73852d541fd023f9b3de
Gihwan Kim <ghkim3221@gmail.com>
2019-01-03 19:05:33 +0900

    2742 : 기찍 N 풀이

---
+++ baekjoon/2742.py
@@ -0,0 +1,15 @@
+"""
+    2742 : 기찍 N
+    URL : https://www.acmicpc.net/problem/2742
+    Input :
+        5
+    Output :
+        5
+        4
+        3
+        2
+        1
+"""
+N = int(input())
+for i in range(N, 0, -1):
+    print(i)
