612812f303c4c1bc83cc84a6d2e8874d7cbdac8a
Gihwan Kim <ghkim3221@gmail.com>
2019-01-03 19:02:06 +0900

    2558 : A+B - 2 풀이

---
+++ baekjoon/2558.py
@@ -0,0 +1,12 @@
+"""
+    2558 : A+B - 2
+    URL : https://www.acmicpc.net/problem/2558
+    Input :
+        1
+        2
+    Output :
+        3
+"""
+A = int(input())
+B = int(input())
+print((A + B))
