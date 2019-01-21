4bc6337bfb2d1de4c9813c03fba593b5cb950b9d
Gihwan Kim <ghkim3221@gmail.com>
2019-01-21 21:24:55 +0900

    11654 : 아스키 코드 풀이

---
+++ baekjoon/11654.py
@@ -0,0 +1,30 @@
+"""
+    11654 : 아스키 코드
+    URL : https://www.acmicpc.net/problem/11654
+    Input #1 :
+        A
+    Output #1 :
+        65
+    Input #2 :
+        C
+    Output #2 :
+        67
+    Input #3 :
+        0
+    Output #3 :
+        48
+    Input #4 :
+        9
+    Output #4 :
+        57
+    Input #5 :
+        a
+    Output #5 :
+        97
+    Input #6 :
+        z
+    Output #6 :
+        122
+"""
+
+print(ord(input()))
