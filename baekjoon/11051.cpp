17d6a0f5e85eb0f8405fdf4983eca427bd2214fb
Gihwan Kim <ghkim3221@gmail.com>
2019-01-28 23:13:18 +0900

    11051 : 이항 계수 2 풀이

---
+++ baekjoon/11051.cpp
@@ -0,0 +1,57 @@
+/*
+    11051 : 이항 계수 2
+    URL : https://www.acmicpc.net/problem/11051
+    Input :
+        5 2
+    Output :
+        10
+*/
+
+#include <iostream>
+#include <cstdint>
+#include <cstring>
+
+#define MAX_N 1001
+#define MAX_K 1001
+
+#define MOD 10007
+
+using namespace std;
+
+uint64_t cache[MAX_N][MAX_K];
+
+uint64_t binomial(int n, int k)
+{
+    uint64_t &ret = cache[n][k];
+
+    if (ret != -1)
+    {
+        return ret;
+    }
+
+    if (k == 0 || k == n)
+    {
+        return 1;
+    }
+
+    ret = ((binomial(n - 1, k - 1) % MOD) + (binomial(n - 1, k) % MOD)) % MOD;
+    return ret;
+}
+
+int main(int argc, char const *argv[])
+{
+    int N;
+    int K;
+
+    memset(&(cache[0]), -1, sizeof(uint64_t) * MAX_N * MAX_K);
+    cache[0][0] = 1;
+    cache[1][0] = 1;
+    cache[1][1] = 1;
+
+    cin >> N;
+    cin >> K;
+
+    cout << binomial(N, K);
+
+    return 0;
+}
