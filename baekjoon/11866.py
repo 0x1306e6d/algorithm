720fa039c4366b216e6f7bb0682ccbf7a43eba60
Gihwan Kim <ghkim3221@gmail.com>
2019-02-07 20:06:52 +0900

    11866 : 조세퍼스 문제 0 풀이

---
+++ baekjoon/11866.py
@@ -0,0 +1,20 @@
+"""
+    11866 : 조세퍼스 문제 0
+    URL : https://www.acmicpc.net/problem/11866
+    Input :
+        7 3
+    Output :
+        <3, 6, 2, 7, 5, 1, 4>
+"""
+
+N, M = map(int, input().split(' '))
+
+i = 0
+josephus = []
+sequence = [i for i in range(1, N + 1)]
+while sequence:
+    i = (i + M - 1) % len(sequence)
+    josephus.append(sequence[i])
+    sequence.remove(sequence[i])
+
+print("<{}>".format(', '.join(str(c) for c in josephus)))
