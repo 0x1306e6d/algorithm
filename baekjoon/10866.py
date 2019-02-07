f13361202813138087a0a39515cfe6f8d0a87153
Gihwan Kim <ghkim3221@gmail.com>
2019-02-07 20:19:55 +0900

    10866 : 덱 풀이

---
+++ baekjoon/10866.py
@@ -0,0 +1,183 @@
+"""
+    10866 : 덱
+    URL : https://www.acmicpc.net/problem/10866
+    Input #1 :
+        15
+        push_back 1
+        push_front 2
+        front
+        back
+        size
+        empty
+        pop_front
+        pop_back
+        pop_front
+        size
+        empty
+        pop_back
+        push_front 3
+        empty
+        front
+    Output #1 :
+        2
+        1
+        2
+        0
+        2
+        1
+        -1
+        0
+        1
+        -1
+        0
+        3
+    Input #2 :
+        22
+        front
+        back
+        pop_front
+        pop_back
+        push_front 1
+        front
+        pop_back
+        push_back 2
+        back
+        pop_front
+        push_front 10
+        push_front 333
+        front
+        back
+        pop_back
+        pop_back
+        push_back 20
+        push_back 1234
+        front
+        back
+        pop_back
+        pop_back
+    Output #2 :
+        -1
+        -1
+        -1
+        -1
+        1
+        1
+        2
+        2
+        333
+        10
+        10
+        333
+        20
+        1234
+        1234
+        20
+"""
+
+
+class Deque:
+    def __init__(self):
+        self._data = []
+
+    def __str__(self):
+        return str(self._data)
+
+    def push_front(self, x):
+        """
+        정수 X를 덱의 앞에 넣는다.
+        """
+        self._data.insert(0, x)
+
+    def push_back(self, x):
+        """
+        정수 X를 덱의 뒤에 넣는다.
+        """
+        self._data.append(x)
+
+    def pop_front(self):
+        """
+        덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다.
+        만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+        """
+        if self.empty():
+            return -1
+        else:
+            first = self._data[0]
+            self._data = self._data[1:]
+            return first
+
+    def pop_back(self):
+        """
+        덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다.
+        만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+        """
+        if self.empty():
+            return -1
+        else:
+            last = self._data[-1]
+            self._data = self._data[:-1]
+            return last
+
+    def size(self):
+        """
+        덱에 들어있는 정수의 개수를 출력한다.
+        """
+        return len(self._data)
+
+    def empty(self):
+        """
+        덱이 비어있으면 1을, 아니면 0을 출력한다.
+        """
+        if self._data:
+            return 0
+        else:
+            return 1
+
+    def front(self):
+        """
+        덱의 가장 앞에 있는 정수를 출력한다.
+        만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+        """
+        if self.empty():
+            return -1
+        else:
+            return self._data[0]
+
+    def back(self):
+        """
+        덱의 가장 뒤에 있는 정수를 출력한다.
+        만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+        """
+        if self.empty():
+            return -1
+        else:
+            return self._data[-1]
+
+
+deque = Deque()
+
+N = int(input())
+for _ in range(N):
+    cmd = input().split(' ')
+    if len(cmd) is 1:
+        cmd = cmd[0]
+    else:
+        x = int(cmd[1])
+        cmd = cmd[0]
+
+    if cmd == 'push_front':
+        deque.push_front(x)
+    elif cmd == 'push_back':
+        deque.push_back(x)
+    elif cmd == 'pop_front':
+        print(deque.pop_front())
+    elif cmd == 'pop_back':
+        print(deque.pop_back())
+    elif cmd == 'size':
+        print(deque.size())
+    elif cmd == 'empty':
+        print(deque.empty())
+    elif cmd == 'front':
+        print(deque.front())
+    elif cmd == 'back':
+        print(deque.back())
