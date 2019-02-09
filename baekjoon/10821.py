43f009d8593bf036f83beb6721e9066fe3941e9c
Gihwan Kim <ghkim3221@gmail.com>
2019-02-09 22:21:19 +0900

    10821 : 정수의 개수 풀이

---
+++ baekjoon/10821.py
@@ -0,0 +1,11 @@
+"""
+    10821 : 정수의 개수
+    URL : https://www.acmicpc.net/problem/10821
+    Input :
+        10,20,30,50,100
+    Output :
+        5
+"""
+
+array = list(map(int, input().split(',')))
+print(len(array))
