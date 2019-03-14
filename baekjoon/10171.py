03334311384f559d0ebeefca59d2868dcc36b028
Gihwan Kim <ghkim3221@gmail.com>
2019-03-14 19:51:58 +0900

    10171 : 고양이 풀이

---
+++ baekjoon/10171.py
@@ -0,0 +1,15 @@
+"""
+    10171 : 고양이
+    URL : https://www.acmicpc.net/problem/10171
+    Input :
+    Output :
+        \    /\
+        )  ( ')
+        (  /  )
+        \(__)|
+"""
+
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
