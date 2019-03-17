253df435075aa3996557cbf9c40bd9be8200faf6
Gihwan Kim <ghkim3221@gmail.com>
2019-03-17 16:14:55 +0900

    13277 : 큰 수 곱셈 풀이

---
+++ baekjoon/13277.py
@@ -0,0 +1,19 @@
+"""
+    13277 : 큰 수 곱셈
+    URL : https://www.acmicpc.net/problem/13277
+    Input #1 :
+        1 2
+    Output #1 :
+        2
+    Input #2 :
+        3 4
+    Output #2 :
+        12
+    Input #3 :
+        893724358493284 238947328947329
+    Output #3 :
+        213553048277135320552236238436
+"""
+
+a, b = map(int, input().split(' '))
+print(a * b)
