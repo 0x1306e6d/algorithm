19a208d479d3837dbe46a37e26a37aab785337d8
Gihwan Kim <ghkim3221@gmail.com>
2019-05-04 21:03:58 +0900

    11365 : !밀비 급일 풀이

--- baekjoon/11365.cpp
+++ baekjoon/11365.cpp
@@ -1,21 +1,41 @@
-#include<iostream>
-#include<string>
+/*
+    11365 : !밀비 급일
+    URL : https://www.acmicpc.net/problem/11365
+    Input :
+		!edoc doog a tahW
+		noitacitsufbo
+		erafraw enirambus detcirtsernu yraurbeF fo tsrif eht no nigeb ot dnetni eW
+		lla sees rodroM fo drol eht ,ssertrof sih nihtiw delaecnoC
+		END
+    Output :
+        What a good code!
+        obfustication
+        We intend to begin on the first of February unrestricted submarine warfare
+        Concealed within his fortress, the lord of Mordor sees all
+*/
 
-int main()
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
 {
 	std::string line;
-	do
+
+	while (true)
 	{
 		std::getline(std::cin, line);
 		if (!line.compare("END"))
 		{
 			break;
 		}
-		for (std::string::reverse_iterator rit = line.rbegin(); rit != line.rend(); ++rit)
+		for (std::string::reverse_iterator rit = line.rbegin();
+			 rit != line.rend();
+			 ++rit)
 		{
 			std::cout << *rit;
 		}
 		std::cout << std::endl;
-	} while (line.compare("END"));
+	}
+
 	return 0;
 }
