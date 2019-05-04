bca44db4f6e6c8119a7d8dfc78ea7fb1cfc3e310
Gihwan Kim <ghkim3221@gmail.com>
2019-05-04 18:29:11 +0900

    10757 : 큰 수 A+B 풀이

---
+++ baekjoon/10757.py
@@ -0,0 +1,11 @@
+"""
+    10757 : 큰 수 A+B
+    URL : https://www.acmicpc.net/problem/10757
+    Input :
+        9223372036854775807 9223372036854775808
+    Output :
+        18446744073709551615
+"""
+
+a, b = map(int, input().split())
+print(a + b)
