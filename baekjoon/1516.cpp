606afe48b2d665095b42312875308902661bbdd0
Gihwan Kim <ghkim3221@gmail.com>
2019-05-22 23:56:39 +0900

    1516 : 게임 개발 풀이

--- baekjoon/1516.cpp
+++ baekjoon/1516.cpp
@@ -18,28 +18,58 @@
 
 #include <iostream>
 #include <algorithm>
+#include <queue>
 #include <vector>
 
+int n;
 std::vector<int> times;
+std::vector<int> indegree;
 std::vector<std::vector<int>> adjacents;
 std::vector<int> answers;
 
-void dfs(int here, int time)
+void tsort()
 {
-    answers[here] += time;
+    std::queue<int> q;
 
-    for (int i = 0; i < adjacents[here].size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        dfs(adjacents[here][i], time);
+        if (indegree[i] == 0)
+        {
+            q.push(i);
+            answers[i] = times[i];
+        }
+    }
+
+    while (!q.empty())
+    {
+        int here = q.front();
+        q.pop();
+
+        for (int i = 0; i < adjacents[here].size(); i++)
+        {
+            int there = adjacents[here][i];
+
+            if (indegree[there] > 0)
+            {
+                answers[there] = std::max(answers[there],
+                                          answers[here] + times[there]);
+
+                indegree[there]--;
+                if (indegree[there] == 0)
+                {
+                    q.push(there);
+                }
+            }
+        }
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    int n;
     std::cin >> n;
 
     times = std::vector<int>(n, 0);
+    indegree = std::vector<int>(n, 0);
     adjacents = std::vector<std::vector<int>>(n, std::vector<int>());
     answers = std::vector<int>(n, 0);
 
@@ -58,15 +88,13 @@ int main(int argc, char const *argv[])
             }
             else
             {
+                indegree[i]++;
                 adjacents[requirement - 1].push_back(i);
             }
         }
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        dfs(i, times[i]);
-    }
+    tsort();
 
     for (int i = 0; i < n; i++)
     {
