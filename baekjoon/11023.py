415b9da5eb449787530c481985a5426765104579
Gihwan Kim <ghkim3221@gmail.com>
2019-10-17 17:39:44 +0900

    11023 : 더하기 3 풀이

---
+++ baekjoon/11023.py
@@ -0,0 +1,14 @@
+"""
+    11023 : 더하기 3
+    URL : https://www.acmicpc.net/problem/11023
+    Input #1 :
+        1 2 3 4 5
+    Output #1 :
+        15
+    Input #2 :
+        5 4 5 4 2 3 1 2
+    Output #2 :
+        26
+"""
+
+print(sum(map(int, input().split())))
