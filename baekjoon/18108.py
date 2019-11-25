7c7fb6788c9ee20a14f316abfd2c62a011c3e04a
Gihwan Kim <ghkim3221@gmail.com>
2019-11-25 16:39:56 +0900

    18108 : 1998년생인 내가 태국에서는 2541년생?!

---
+++ baekjoon/18108.py
@@ -0,0 +1,11 @@
+"""
+    18108 : 1998년생인 내가 태국에서는 2541년생?!
+    URL : https://www.acmicpc.net/problem/18108
+    Input :
+        2541
+    Output :
+        1998
+"""
+
+y = int(input())
+print(y - 543)
