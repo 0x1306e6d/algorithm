009cd4fadaf9fa254f2f2a0a4393dca339287fb1
Gihwan Kim <ghkim3221@gmail.com>
2019-12-05 20:46:23 +0900

    17419 : 비트가 넘쳐흘러 풀이

---
+++ baekjoon/17419.py
@@ -0,0 +1,13 @@
+"""
+    17419 : 비트가 넘쳐흘러
+    URL : https://www.acmicpc.net/problem/17419
+    Input :
+        5
+        10110
+    Output :
+        3
+"""
+
+input()
+k = input()
+print(k.count('1'))
