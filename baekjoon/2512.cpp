b0a803d67fb5732c684ba171243fa7903f37f541
Gihwan Kim <ghkim3221@gmail.com>
2019-12-11 16:46:54 +0900

    2512: 예산 풀이

--- baekjoon/2512.cpp
+++ baekjoon/2512.cpp
@@ -1,62 +1,89 @@
 /*
-    2512 : 예산
-    URL : https://www.acmicpc.net/problem/2512
-    Input :
+    2512: 예산
+    URL: https://www.acmicpc.net/problem/2512
+    Input:
 		4
 		120 110 140 150
 		485
-    Output :
+    Output:
 		127
 */
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
-using namespace std;
-
-int main()
+int main(int argc, char const *argv[])
 {
-	int N, M;
-	int sum = 0, big = 0;
-	int *arr;
-	cin >> N;
-	arr = new int[N];
-	for (int i = 0; i < N; ++i)
-	{
-		int n;
-		cin >> n;
-		arr[i] = n;
-		sum += n;
-		if (n > big)
-		{
-			big = n;
-		}
-	}
-	cin >> M;
+	std::vector<int> budgets;
 
-	if (sum <= M)
+	int n;
+	std::cin >> n;
+
+	int sumBudget = 0;
+	for (int i = 0; i < n; i++)
 	{
-		cout << big;
-		return 0;
+		int budget;
+		std::cin >> budget;
+
+		sumBudget += budget;
+
+		budgets.push_back(budget);
 	}
 
-	bool flag = false;
-	int average = M / N;
-	int remain = M;
-	int count = 0;
-	for (int i = 0; i < N; ++i)
-	{
-		if (arr[i] <= average)
-		{
-			remain -= arr[i];
-		}
-		else
-		{
-			count++;
-			remain -= average;
-		}
-	}
+	std::sort(budgets.begin(), budgets.end());
 
-	cout << (average + (remain / count)) << endl;
+	int m;
+	std::cin >> m;
+
+	if (sumBudget <= m)
+	{
+		std::cout << budgets.back();
+	}
+	else
+	{
+		int sum = 0;
+		int maxBudget = 0;
+		int maxBudgetLimit = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			int remainingBudget = (m - sum);
+			if (remainingBudget < 0)
+			{
+				break;
+			}
+
+			int budgetLimit = (remainingBudget / (n - i));
+			int unusedBudget = 0;
+			for (int j = 0; j < n; j++)
+			{
+				if (budgetLimit < budgets[j])
+				{
+					break;
+				}
+				else
+				{
+					unusedBudget += (budgetLimit - budgets[j]);
+				}
+			}
+
+			int usedBudget = ((budgetLimit * n) - unusedBudget);
+			if (usedBudget > maxBudget)
+			{
+				maxBudget = usedBudget;
+				maxBudgetLimit = budgetLimit;
+			}
+			else if (usedBudget == maxBudget)
+			{
+				maxBudgetLimit = std::max(maxBudgetLimit, budgetLimit);
+			}
+
+			sum += budgets[i];
+		}
+
+		std::cout << maxBudgetLimit;
+	}
 
 	return 0;
 }
