3313760730d1d0db91e7d1ae4cce5863de294aa7
Gihwan Kim <ghkim3221@gmail.com>
2019-12-16 16:51:14 +0900

    3046 : R2 풀이

---
+++ baekjoon/3046.py
@@ -0,0 +1,12 @@
+"""
+    3046 : R2
+    URL : https://www.acmicpc.net/problem/3046
+    Input :
+        11 15
+    Output :
+        19
+"""
+
+r1, s = map(int, input().split())
+r2 = (2 * s) - r1
+print(r2)
