97fb9584c9960cf64af494a3ae45f3ae755dd836
Gihwan Kim <ghkim3221@gmail.com>
2020-01-25 19:12:15 +0900

    Add upper_bound algorithm snippet

---
+++ snippets/algorithm/upper_bound.py
@@ -0,0 +1,34 @@
+import unittest
+import bisect
+
+
+def upper_bound(arr, n):
+    low = 0
+    high = len(arr)
+
+    while low < high:
+        mid = (low + high) // 2
+        if arr[mid] > n:
+            high = mid
+        else:
+            low = mid + 1
+
+    return low
+
+
+class UpperBoundTestCase(unittest.TestCase):
+    def test_found(self):
+        self.assertEqual(upper_bound([1, 3, 5, 7, 7, 8], 7), 5)
+
+    def test_not_found(self):
+        self.assertEqual(upper_bound([1, 2, 3, 5, 7, 9, 11, 15], 6), 4)
+
+    def test_too_small_input(self):
+        self.assertEqual(upper_bound([1, 2, 3, 4, 5], 0), 0)
+
+    def test_too_big_input(self):
+        self.assertEqual(upper_bound([1, 2, 3, 4, 5], 7), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
