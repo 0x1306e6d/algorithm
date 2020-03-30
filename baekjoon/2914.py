d2c111514c9b720769e9a14af07562a60b81c8f1
Gihwan Kim <ghkim3221@gmail.com>
2020-03-30 19:51:45 +0900

    Solve 저작권 (2914)

---
+++ baekjoon/2914.py
@@ -0,0 +1,11 @@
+"""
+    2914 : 저작권
+    URL : https://www.acmicpc.net/problem/2914
+    Input :
+        38 24
+    Output :
+        875
+"""
+
+a, i = map(int, input().split())
+print((a * (i - 1)) + 1)
