a8ef9b7768f560766f301a461e88f0d35a1ec278
Gihwan Kim <ghkim3221@gmail.com>
2020-05-21 22:28:23 +0900

    Solve 긴자리 계산 (2338)

---
+++ baekjoon/2338.py
@@ -0,0 +1,18 @@
+"""
+    2338 : 긴자리 계산
+    URL : https://www.acmicpc.net/problem/2338
+    Input :
+        1
+        -1
+    Output :
+        0
+        2
+        -1
+"""
+
+a = int(input())
+b = int(input())
+
+print(a + b)
+print(a - b)
+print(a * b)
