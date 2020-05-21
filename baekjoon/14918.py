4c0ff72503c26e7cd180aeb206e3edc538db775f
Gihwan Kim <ghkim3221@gmail.com>
2020-05-21 22:34:53 +0900

    Solve 더하기 (14918)

---
+++ baekjoon/14918.py
@@ -0,0 +1,15 @@
+"""
+    14918 : 더하기
+    URL : https://www.acmicpc.net/problem/14918
+    Input #1 :
+        1 2
+    Output #1 :
+        3
+    Input #2 :
+        4 5
+    Output #2 :
+        9
+"""
+
+a, b = map(int, input().split())
+print(a + b)
