621fb857d1c1ccdd612ef5e8a7fa9e4f341bf9a8
Gihwan Kim <ghkim3221@gmail.com>
2020-07-25 15:06:39 +0900

    Solve The K Weakest Rows in a Matrix (1337)

---
+++ leetcode/1337.py
@@ -0,0 +1,47 @@
+import unittest
+
+from typing import List
+
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        rows = []
+        for i, row in enumerate(mat):
+            soldiers = row.count(1)
+            rows.append((soldiers, i))
+
+        ordered = list(map(lambda pair: pair[1], sorted(rows)))
+        return ordered[:k]
+
+
+class SolutionTestCase(unittest.TestCase):
+    def test_example1(self):
+        # Input
+        mat = [[1, 1, 0, 0, 0],
+               [1, 1, 1, 1, 0],
+               [1, 0, 0, 0, 0],
+               [1, 1, 0, 0, 0],
+               [1, 1, 1, 1, 1]]
+        k = 3
+        # Output
+        output = [2, 0, 3]
+
+        solution = Solution()
+        self.assertEqual(solution.kWeakestRows(mat, k), output)
+
+    def test_example2(self):
+        # Input
+        mat = [[1, 0, 0, 0],
+               [1, 1, 1, 1],
+               [1, 0, 0, 0],
+               [1, 0, 0, 0]]
+        k = 2
+        # Output
+        output = [0, 2]
+
+        solution = Solution()
+        self.assertEqual(solution.kWeakestRows(mat, k), output)
+
+
+if __name__ == "__main__":
+    unittest.main()
