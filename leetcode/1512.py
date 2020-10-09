f1d5a06a7acf733df60f77a5b10d5d3686335393
Gihwan Kim <ghkim3221@gmail.com>
2020-10-09 18:37:53 +0900

    Solve Number of Good Pairs (1512)

---
+++ leetcode/1512.py
@@ -0,0 +1,20 @@
+"""
+    File: 1512.py
+    Title: Number of Good Pairs
+    Difficulty: Easy
+    URL: https://leetcode.com/problems/number-of-good-pairs/
+"""
+
+import unittest
+
+from typing import List
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    count += 1
+        return count
