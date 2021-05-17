4c01aa99e5a069b961d2495058b3565339ba71d0
Gihwan Kim <ghkim3221@gmail.com>
2021-05-17 22:16:26 +0900

    Solve Big Countries (595)

---
+++ leetcode/595.sql
@@ -0,0 +1,8 @@
+-- File: 595.sql
+-- Title: Big Countries
+-- Difficulty: Easy
+-- URL: https://leetcode.com/problems/big-countries/
+
+SELECT name, population, area
+FROM World
+WHERE area > 3000000 OR population > 25000000
