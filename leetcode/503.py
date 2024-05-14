204f99eb2a511e296a3cf66cfab2fb5dd09278cc
Gihwan Kim <ghkim3221@gmail.com>
2024-05-14 20:48:23 +0900

    Solve Next Greater Element II (503)

--- leetcode/503.py
+++ leetcode/503.py
@@ -5,43 +5,20 @@
     URL: https://leetcode.com/problems/next-greater-element-ii/
 """
 
-import unittest
-
 from typing import List
 
 
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        ans = [-1] * len(nums)
         stack = []
-        for i in range(2 * len(nums)):
-            j = i % len(nums)
-            while stack and nums[j] > nums[stack[-1]]:
-                ans[stack[-1]] = nums[j]
-                stack.pop()
-            stack.append(j)
+        ans = [-1] * len(nums)
+        for i, n in enumerate(nums):
+            while stack and stack[-1][0] < n:
+                _, j = stack.pop()
+                ans[j] = n
+            stack.append((n, i))
+        for i, n in enumerate(nums):
+            while stack and stack[-1][0] < n:
+                _, j = stack.pop()
+                ans[j] = n
         return ans
-
-
-class SolutionTestCase(unittest.TestCase):
-    def test_example1(self):
-        # Input
-        nums = [1, 2, 1]
-        # Output
-        output = [2, -1, 2]
-
-        solution = Solution()
-        self.assertEqual(solution.nextGreaterElements(nums), output)
-
-    def test_example2(self):
-        # Input
-        nums = [1, 2, 3, 4, 3]
-        # Output
-        output = [2, 3, 4, -1, 4]
-
-        solution = Solution()
-        self.assertEqual(solution.nextGreaterElements(nums), output)
-
-
-if __name__ == "__main__":
-    unittest.main()
