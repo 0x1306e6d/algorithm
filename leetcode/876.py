3197a68f8823ddb8ef26aa521bae260c1764f070
Gihwan Kim <ghkim3221@gmail.com>
2024-05-14 22:11:37 +0900

    Solve Middle of the Linked List (876)

---
+++ leetcode/876.py
@@ -0,0 +1,23 @@
+"""
+    File: 876.py
+    Title: Middle of the Linked List
+    Difficulty: Easy
+"""
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
