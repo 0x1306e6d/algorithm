028f870787bab689957eaa84f3953f3b58faeca7
Gihwan Kim <ghkim3221@gmail.com>
2024-05-17 06:37:59 +0900

    Solve Count Vowels Permutation (1220)

--- leetcode/1220.py
+++ leetcode/1220.py
@@ -5,84 +5,17 @@
     URL: https://leetcode.com/problems/count-vowels-permutation/
 """
 
-import unittest
-
-
-from collections import defaultdict
-
 
 class Solution:
-
-    MOD = 10**9 + 7
-
-    def __init__(self):
-        self.cache = defaultdict(dict)
-
     def countVowelPermutation(self, n: int) -> int:
-        return self.count(n)
-
-    def count(self, n: int, vowel: str = None) -> int:
-        if n == 0:
-            return 1
-
-        if vowel in self.cache[n]:
-            return self.cache[n][vowel]
-
-        count = 0
-        if vowel == 'a':
-            count = self.count(n - 1, 'e')
-        elif vowel == 'e':
-            count = self.count(n - 1, 'a')
-            count += self.count(n - 1, 'i')
-        elif vowel == 'i':
-            count = self.count(n - 1, 'a')
-            count += self.count(n - 1, 'e')
-            count += self.count(n - 1, 'o')
-            count += self.count(n - 1, 'u')
-        elif vowel == 'o':
-            count = self.count(n - 1, 'i')
-            count += self.count(n - 1, 'u')
-        elif vowel == 'u':
-            count = self.count(n - 1, 'a')
-        elif vowel is None:
-            count = self.count(n - 1, 'a')
-            count += self.count(n - 1, 'e')
-            count += self.count(n - 1, 'i')
-            count += self.count(n - 1, 'o')
-            count += self.count(n - 1, 'u')
-        count %= self.MOD
-        self.cache[n][vowel] = count
-        return count
-
-
-class SolutionTestCase(unittest.TestCase):
-    def test_example1(self):
-        # Input
-        n = 1
-        # Output
-        output = 5
-
-        solution = Solution()
-        self.assertEqual(solution.countVowelPermutation(n), output)
-
-    def test_example2(self):
-        # Input
-        n = 2
-        # Output
-        output = 10
-
-        solution = Solution()
-        self.assertEqual(solution.countVowelPermutation(n), output)
-
-    def test_example3(self):
-        # Input
-        n = 5
-        # Output
-        output = 68
-
-        solution = Solution()
-        self.assertEqual(solution.countVowelPermutation(n), output)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        mod = 10**9 + 7
+        dp = [[0] * 5 for _ in range(1 + n)]
+        for i in range(5):
+            dp[1][i] = 1
+        for i in range(1, n + 1):
+            dp[i][0] += (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4]) % mod
+            dp[i][1] += (dp[i - 1][0] + dp[i - 1][2]) % mod
+            dp[i][2] += (dp[i - 1][1] + dp[i - 1][3]) % mod
+            dp[i][3] += (dp[i - 1][2]) % mod
+            dp[i][4] += (dp[i - 1][2] + dp[i - 1][3]) % mod
+        return sum(dp[n]) % mod
