ff289013f19de94933a09f91c569f135df7e544a
Gihwan Kim <ghkim3221@gmail.com>
2024-05-16 23:59:14 +0900

    Solve Unique Paths (62)

--- leetcode/62.py
+++ leetcode/62.py
@@ -5,70 +5,19 @@
     URL: https://leetcode.com/problems/unique-paths/
 """
 
-import unittest
-
 
 class Solution:
-    def __init__(self):
-        self.cache = {}
-
     def uniquePaths(self, m: int, n: int) -> int:
-        return self.comb(m + n - 2, m - 1)
-
-    def comb(self, n: int, r: int) -> int:
-        if n in self.cache:
-            if r in self.cache[n]:
-                return self.cache[n][r]
-        else:
-            self.cache[n] = {}
-
-        if n == r or r == 0:
+        if m == 1 or n == 1:
             return 1
-        self.cache[n][r] = self.comb(n - 1, r - 1) + self.comb(n - 1, r)
-        return self.cache[n][r]
+        if n > m:
+            return self.uniquePaths(n, m)
 
-
-class SolutionTestCase(unittest.TestCase):
-    def test_example1(self):
-        # Input
-        m = 3
-        n = 7
-        # Output
-        output = 28
-
-        solution = Solution()
-        self.assertEqual(solution.uniquePaths(m, n), output)
-
-    def test_example2(self):
-        # Input
-        m = 3
-        n = 2
-        # Output
-        output = 3
-
-        solution = Solution()
-        self.assertEqual(solution.uniquePaths(m, n), output)
-
-    def test_example3(self):
-        # Input
-        m = 7
-        n = 3
-        # Output
-        output = 28
-
-        solution = Solution()
-        self.assertEqual(solution.uniquePaths(m, n), output)
-
-    def test_example4(self):
-        # Input
-        m = 3
-        n = 3
-        # Output
-        output = 6
-
-        solution = Solution()
-        self.assertEqual(solution.uniquePaths(m, n), output)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        memo = [0] * (m + n + 1)
+        memo[1] = 1
+        memo[2] = 2
+        for i in range(3, m + n + 1):
+            memo[i] = memo[i - 1] * i
+        l = m + n - 2
+        r = m - 1
+        return memo[l] // (memo[l - r] * memo[r])
