e5f7c3d07c05359651782c357770f2cfbc7310b1
Gihwan Kim <ghkim3221@gmail.com>
2024-05-17 00:20:42 +0900

    Solve Best Time to Buy and Sell Stock with Transaction Fee (714)

--- leetcode/714.py
+++ leetcode/714.py
@@ -5,42 +5,19 @@
     URL: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
 """
 
-import unittest
-
+from functools import lru_cache
 from typing import List
 
 
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        buy = -prices[0]
-        sell = 0
-        for i in range(1, len(prices)):
-            buy = max(buy, sell - prices[i])
-            sell = max(sell, buy + prices[i] - fee)
-        return sell
+        @lru_cache
+        def dp(i, h):
+            if i >= len(prices):
+                return 0
+            if h:
+                return max(-fee + prices[i] + dp(i + 1, False), dp(i + 1, True))
+            else:
+                return max(-prices[i] + dp(i + 1, True), dp(i + 1, False))
 
-
-class SolutionTestCase(unittest.TestCase):
-    def test_example1(self):
-        # Input
-        prices = [1, 3, 2, 8, 4, 9]
-        fee = 2
-        # Output
-        output = 8
-
-        solution = Solution()
-        self.assertEqual(solution.maxProfit(prices, fee), output)
-
-    def test_example2(self):
-        # Input
-        prices = [1, 3, 7, 5, 10, 3]
-        fee = 3
-        # Output
-        output = 6
-
-        solution = Solution()
-        self.assertEqual(solution.maxProfit(prices, fee), output)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        return dp(0, False)
