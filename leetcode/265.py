1b7f69d1936d795003d815c101720437d4637968
Gihwan Kim <ghkim3221@gmail.com>
2024-05-17 00:43:20 +0900

    Solve Paint House II (265)

--- leetcode/265.py
+++ leetcode/265.py
@@ -12,14 +12,22 @@ class Solution:
         inf = float("inf")
         n, k = len(costs), len(costs[0])
         dp = [[inf] * k for _ in range(n)]
+
+        first, second = -1, -1
         for i in range(k):
             dp[0][i] = costs[0][i]
+            if dp[0][i] < dp[0][second]:
+                second = i
+            if dp[0][i] < dp[0][first]:
+                first, second = i, first
+
         for i in range(1, n):
-            for j1 in range(k):
-                cost = inf
-                for j2 in range(k):
-                    if j1 == j2:
-                        continue
-                    cost = min(cost, dp[i - 1][j2])
-                dp[i][j1] = costs[i][j1] + cost
+            new_first, new_second = -1, -1
+            for j in range(k):
+                dp[i][j] = costs[i][j] + dp[i - 1][first if first != j else second]
+                if dp[i][j] < dp[i][new_second]:
+                    new_second = j
+                if dp[i][j] < dp[i][new_first]:
+                    new_first, new_second = j, new_first
+            first, second = new_first, new_second
         return min(dp[-1])
