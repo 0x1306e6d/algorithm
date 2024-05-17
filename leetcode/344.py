3c5814ca398506aae7d1d4f9ae45b25a81731cde
Gihwan Kim <ghkim3221@gmail.com>
2024-05-17 21:13:28 +0900

    Solve Reverse String (344)

---
+++ leetcode/344.py
@@ -0,0 +1,19 @@
+"""
+    File: 344.py
+    Title: Reverse String
+    Difficulty: Easy
+"""
+
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        i, j = 0, len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
