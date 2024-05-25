3137f19ee868ee88c39fb27ff22eab967d4c0198
Gihwan Kim <ghkim3221@gmail.com>
2024-05-25 18:16:28 +0900

    Solve Binary Search Tree Iterator (173)

---
+++ leetcode/173.py
@@ -0,0 +1,43 @@
+"""
+    File: 173.py
+    Title: Binary Search Tree Iterator
+    Difficulty: Medium
+"""
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.stack = []
+        self._build_stack(root)
+
+    def _build_stack(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self) -> int:
+        node = self.stack.pop()
+        if node.right:
+            self._build_stack(node.right)
+        return node.val
+
+    def hasNext(self) -> bool:
+        if self.stack:
+            return True
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
