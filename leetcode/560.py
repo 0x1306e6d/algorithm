4c8df0700ffcc9f3e7480e1de37c19d5582f411b
Gihwan Kim <ghkim3221@gmail.com>
2024-05-27 22:31:54 +0900

    Solve Subarray Sum Equals K (560)

--- leetcode/560.py
+++ leetcode/560.py
@@ -5,50 +5,21 @@
     URL: https://leetcode.com/problems/subarray-sum-equals-k/
 """
 
-import unittest
-
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        memo = {}
-
-        count = 0
-        s = 0
+        ans = 0
+        current_sum = 0
+        prefix_sum = defaultdict(int)
         for n in nums:
-            s += n
-            if s == k:
-                count += 1
-            if s - k in memo:
-                count += memo[s - k]
-            if s not in memo:
-                memo[s] = 0
-            memo[s] += 1
-        return count
+            current_sum += n
 
+            if current_sum == k:
+                ans += 1
+            ans += prefix_sum[current_sum - k]
 
-class SolutionTestCase(unittest.TestCase):
-    def test_example1(self):
-        # Input
-        nums = [1, 1, 1]
-        k = 2
-        # Output
-        output = 2
-
-        solution = Solution()
-        self.assertEqual(solution.subarraySum(nums, k), output)
-
-    def test_example2(self):
-        # Input
-        nums = [1, 2, 3]
-        k = 3
-        # Output
-        output = 2
-
-        solution = Solution()
-        self.assertEqual(solution.subarraySum(nums, k), output)
-
-
-if __name__ == "__main__":
-    unittest.main()
+            prefix_sum[current_sum] += 1
+        return ans
