8cf0f9e0e87fad59d3dd21d7459370e42795956f
Gihwan Kim <ghkim3221@gmail.com>
2024-06-04 23:00:46 +0900

    Solve Linked List Cycle II (142)

---
+++ leetcode/142.py
@@ -0,0 +1,38 @@
+"""
+    File: 142.py
+    Title: Linked List Cycle II
+    Difficulty: Medium
+"""
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+
+        slow = fast = head
+        cycle = False
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                cycle = True
+                break
+        if not cycle:
+            return None
+
+        a = head
+        b = fast
+        while a != b:
+            a = a.next
+            b = b.next
+        return a
