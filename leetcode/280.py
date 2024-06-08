7b248c9c8a706b7aa5428b54b22e6c9c310852d8
Gihwan Kim <ghkim3221@gmail.com>
2024-06-08 18:40:55 +0900

    Solve Wiggle Sort (280)

---
+++ leetcode/280.py
@@ -0,0 +1,17 @@
+"""
+    File: 280.py
+    Title: Wiggle Sort
+    Difficulty: Medium
+"""
+
+from typing import List
+
+
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+        for i in range(1, len(nums) - 1, 2):
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
